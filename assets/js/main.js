8be00ab0cda6a60f09f6aa79cfcbb330b186a6a2
adrian <revanskyw@gmail.com>
2022-08-17 16:26:36 -0400

    implementacion de funcionalidad lectura input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,37 @@
-let nameCard = document.getElementById("name-card");
-let numberCard;
-let monthCard;
-let yearCard;
-let cvcCard;
+// const formCard = document.querySelector("#form-card");
 
-function algo() {
-  console.log(nameCard.value);
-}
-algo();
+let nameCard = document.getElementById("name-card");
+let numberCard = document.getElementById("card-number");
+let infoMonth = document.getElementById("info-month");
+let infoYear = document.getElementById("info-year");
+let infoCvc = document.getElementById("info-cvc");
+
+nameCard.addEventListener("keyup", (event) => {
+  let inputText = event.path[0].value;
+
+  document.querySelector(".texto_tarjeta").innerHTML = inputText;
+});
+
+numberCard.addEventListener("keyup", (event) => {
+  let inputNumber = event.path[0].value;
+
+  document.querySelector(".texto_tarjeta2").innerHTML = inputNumber;
+});
+
+infoMonth.addEventListener("keyup", (event) => {
+  let inputMonth = event.path[0].value;
+
+  document.querySelector(".texto_tarjeta3").innerHTML = inputMonth;
+});
+
+infoYear.addEventListener("keyup", (event) => {
+  let inputYear = event.path[0].value;
+
+  document.querySelector(".texto_tarjeta4").innerHTML = inputYear;
+});
+
+infoCvc.addEventListener("keyup", (event) => {
+  let inputCvc = event.path[0].value;
+
+  document.querySelector(".texto_tarjeta5").innerHTML = inputCvc;
+});
