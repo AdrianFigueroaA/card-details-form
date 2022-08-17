875a88b284aed8785a49ca78c6b0b6b8d853556e
adrian <revanskyw@gmail.com>
2022-08-17 17:20:01 -0400

    actualizacion script lectura input

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,31 +7,31 @@ let infoYear = document.getElementById("info-year");
 let infoCvc = document.getElementById("info-cvc");
 
 nameCard.addEventListener("keyup", (event) => {
-  let inputText = event.path[0].value;
+  let inputText = event.composedPath()[0].value;
 
   document.querySelector(".texto_tarjeta").innerHTML = inputText;
 });
 
 numberCard.addEventListener("keyup", (event) => {
-  let inputNumber = event.path[0].value;
+  let inputNumber = event.composedPath()[0].value;
 
   document.querySelector(".texto_tarjeta2").innerHTML = inputNumber;
 });
 
 infoMonth.addEventListener("keyup", (event) => {
-  let inputMonth = event.path[0].value;
+  let inputMonth = event.composedPath()[0].value;
 
   document.querySelector(".texto_tarjeta3").innerHTML = inputMonth;
 });
 
 infoYear.addEventListener("keyup", (event) => {
-  let inputYear = event.path[0].value;
+  let inputYear = event.composedPath()[0].value;
 
   document.querySelector(".texto_tarjeta4").innerHTML = inputYear;
 });
 
 infoCvc.addEventListener("keyup", (event) => {
-  let inputCvc = event.path[0].value;
+  let inputCvc = event.composedPath()[0].value;
 
   document.querySelector(".texto_tarjeta5").innerHTML = inputCvc;
 });
