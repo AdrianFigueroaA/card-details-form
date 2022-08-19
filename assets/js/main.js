1bf627e310117348e4240a170716885ee1c1aa93
adrian <revanskyw@gmail.com>
2022-08-19 18:08:51 -0400

    se implementa funcionalidad de data en tarjetas

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,29 +9,29 @@ let infoCvc = document.getElementById("info-cvc");
 nameCard.addEventListener("keyup", (event) => {
   let inputText = event.composedPath()[0].value;
 
-  document.querySelector(".texto_tarjeta").innerHTML = inputText;
+  document.querySelector(".texto_name").innerHTML = inputText;
 });
 
 numberCard.addEventListener("keyup", (event) => {
   let inputNumber = event.composedPath()[0].value;
 
-  document.querySelector(".texto_tarjeta2").innerHTML = inputNumber;
+  document.querySelector(".texto_number").innerHTML = inputNumber;
 });
 
 infoMonth.addEventListener("keyup", (event) => {
   let inputMonth = event.composedPath()[0].value;
 
-  document.querySelector(".texto_tarjeta3").innerHTML = inputMonth;
+  document.querySelector(".texto_month").innerHTML = inputMonth;
 });
 
 infoYear.addEventListener("keyup", (event) => {
   let inputYear = event.composedPath()[0].value;
 
-  document.querySelector(".texto_tarjeta4").innerHTML = inputYear;
+  document.querySelector(".texto_year").innerHTML = inputYear;
 });
 
 infoCvc.addEventListener("keyup", (event) => {
   let inputCvc = event.composedPath()[0].value;
 
-  document.querySelector(".texto_tarjeta5").innerHTML = inputCvc;
+  document.querySelector(".texto_cvc").innerHTML = inputCvc;
 });
